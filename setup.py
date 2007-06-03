329d83f1c2351cbdac6515bccd909cdfb147c5f9
Stephan Richter <stephan.richter@gmail.com>
2007-06-03 17:14:54 +0000

    Record recent changes.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.formui',
-    version='1.0.0',
+    version='1.0.1',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A set of initial UI components for z3c.form.",
