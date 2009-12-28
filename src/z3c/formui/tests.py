22dfaf3e4e07872464096f6f1b516b6936695214
Michael Howitz <mh@gocept.com>
2009-12-28 10:26:26 +0000

    Fixed tests so they do not break for `z3c.form` 2.2.0.

--- src/z3c/formui/tests.py
+++ src/z3c/formui/tests.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2005 Zope Foundation and Contributors.
+# Copyright (c) 2005-2009 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -16,30 +16,35 @@ $Id: tests.py 72087 2007-01-18 01:03:33Z rogerineichen $
 """
 __docformat__ = "reStructuredText"
 
-import unittest
 from zope.testing import doctest
+import unittest
+import z3c.form.outputchecker
+import z3c.form.testing
 
-from z3c.form import testing
 
 def setUpZPT(test):
-    testing.setUpZPT(test)
+    z3c.form.testing.setUpZPT(test)
     from zope.app.pagetemplate.metaconfigure import registerType
     from zope.contentprovider.tales import TALESProviderExpression
     from z3c.macro.tales import MacroExpression
     registerType('macro', MacroExpression)
     registerType('provider', TALESProviderExpression)
 
+
 def setUpZ3CPT(test):
-    testing.setUpZ3CPT(test)
+    z3c.form.testing.setUpZ3CPT(test)
     from zope.component import provideUtility
     from z3c.macro.tales import z3cpt_macro_expression
     provideUtility(z3cpt_macro_expression, name='macro')
 
+
 def test_suite():
     return unittest.TestSuite([
-        doctest.DocFileSuite('README.txt',
-            setUp=setUp, tearDown=testing.tearDown,
+        doctest.DocFileSuite(
+            'README.txt',
+            setUp=setUp,
+            tearDown=z3c.form.testing.tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
-            checker=testing.OutputChecker(doctest))
+            checker=z3c.form.outputchecker.OutputChecker(doctest))
         #for setUp in (setUpZPT, setUpZ3CPT)]) # XXX: broken macro tests
         for setUp in (setUpZPT, )])
