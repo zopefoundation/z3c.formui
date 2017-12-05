394907824b4056708638971e7318f18bea06f51e
Hugo <hugovk@users.noreply.github.com>
2017-12-05 21:44:13 +0200

    Add support for Python 3.4-3.6, drop 2.6 and 3.3 (#3)
    * Add support for Python 3.4-3.6, drop 2.6 and 3.3
    * Use Tox-Travis

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name='z3c.formui',
-    version='3.0.1.dev0',
+    version='4.0.0.dev0',
     author="Stephan Richter, Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="A set of initial UI components for z3c.form.",
@@ -44,10 +44,11 @@ setup(
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Natural Language :: English',
         'Operating System :: OS Independent',
@@ -61,6 +62,7 @@ setup(
     extras_require=dict(
         test=['z3c.form [test]'],
         ),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
         'setuptools',
         'z3c.form >= 2.2.0',
