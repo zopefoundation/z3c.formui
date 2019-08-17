424b77205bedefb75180ca90a5d6f57dacf6b1c2
Jürgen Gmach <juergen.gmach@googlemail.com>
2019-08-17 19:39:34 +0200

    Update Python versions.
    - Dropped support for Python 3.4.
    - Added support for Python 3.8.
    
    modified:   .travis.yml
    modified:   CHANGES.txt
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -47,10 +47,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Natural Language :: English',
         'Operating System :: OS Independent',
@@ -64,7 +64,7 @@ setup(
     extras_require=dict(
         test=['z3c.form[test]'],
     ),
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
         'setuptools',
         'z3c.form >= 2.2.0',
